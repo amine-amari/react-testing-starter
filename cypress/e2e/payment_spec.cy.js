697322dea245689e33125d74d6b807d2e9e3cc3c
Amine Amari <amari.mohamed.amine90@gmail.com>
2023-01-22 16:20:07 +0100

    implement payment verification in e2e test

--- cypress/e2e/payment_spec.cy.js
+++ cypress/e2e/payment_spec.cy.js
@@ -22,17 +22,31 @@ describe('payment', () => {
     cy.findByText(/devon becker/i).click();
 
     // add amout and note and click pay
-    cy.findByPlaceholderText(/amount/i).type("5");
+    const paymentAmount = '5.00';
+    cy.findByPlaceholderText(/amount/i).type(paymentAmount);
     const note = uuidv4();
     cy.findByPlaceholderText(/add a note/i).type(note);
     cy.findByRole('button', {  name: /pay/i,  hidden: true}).click();
 
     // return to transactions
     cy.findByRole('button', {  name: /return to transactions/i,  hidden: true}).click();
-    
+
     // go to personal payments
+    cy.findByRole('tab', {  name: /mine/i,  hidden: true}).click();
+
     // click on payment
+    cy.findByText(note).click({ force: true });
+
     // verify if payment was made
+    cy.findByText(`-$${paymentAmount}`).should('be.visible');
+    cy.findByText(note).should('be.visible');
+
     // verify if payment amount was deducted
+    cy.get('[data-test="sidenav-user-balance"]').then($balance => {
+      const convertedOldBalance = parseFloat(oldBalance.replace(/\$|,/g, ""));
+      const convertedNewBalance = parseFloat($balance.text().replace(/\$|,/g, ""));
+      expect (convertedOldBalance - convertedNewBalance ).to.equal(parseFloat(paymentAmount));
+  });
+
   });
 })
